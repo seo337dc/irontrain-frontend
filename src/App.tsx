19f2c6c0815ad32e00d15b0862fd060fd0b7f8b4
seo337dc <sdc337dc@gmail.com>
2025-01-15 21:38:10 +0900

    add : 스타일컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
 
 const App: React.FC = () => {
-  return <h1 className="w-screen h-screen px-12">AAAA</h1>;
+  return (
+    <div className="w-screen h-screen px-12">
+      <Text>AAAA</Text>
+    </div>
+  );
 };
 
 export default App;
+
+const Text = styled.h1`
+  font-size: 18px;
+  font-weight: 700;
+`;
