30b7ecc5d0ced114688be560be1e168b7b9e0a38
seo337dc <sdc337dc@gmail.com>
2025-01-16 20:30:36 +0900

    fix : 기타 수정

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,8 +1,7 @@
 // TODO : 다 제거하고 Response Error 타입 지정ㅊ
-export type TGender = "female" | "male";
 
 export interface RequestModel {
   quantity: number;
-  gender: TGender;
+  // gender: TGender;
   startDate: string;
 }
